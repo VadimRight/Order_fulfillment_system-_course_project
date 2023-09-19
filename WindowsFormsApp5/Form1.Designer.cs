147791fdc499d99dea0d4261f34c4c44ca11ebd2
vadim1vadim2vadim3 <vadim.komashko@bk.ru>
2023-09-19 09:41:28 +0300

    my working student project (Oreder fulfillment system) WindowsFormsApp5 by VisualStudio with charts, connected database

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(325, 208);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(366, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(393, 208);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 226);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(410, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(405, 256);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(764, 261);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(12, 256);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(364, 262);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(428, 226);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(741, 24);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged_1);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(765, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(404, 208);
+            this.dataGridView3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 518);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Client> clients = new List<Client>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SQLiteConnection connection = new SQLiteConnection("Integrated Security=SSPI;Data Source=C.db");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = @"SELECT o.CustomerID, o.LastName, SUM(o.UnitPrice * o.Quantity) AS OrderTotal FROM Orders o INNER JOIN Customers c ON o.CustomerID = c.CustomerID INNER JOIN Products p ON o.ProductID = p.ProductID GROUP BY o.CustomerID, o.LastName";
+
+            using (var rd1 = command.ExecuteReader())
+            {
+                while (rd1.Read())
+                {
+                    string clientID = rd1.GetString(0);
+                    string lastName = rd1.GetString(1);
+                    decimal orderTotal = rd1.GetDecimal(2);
+
+                    clients.Add(new Client(clientID, lastName, orderTotal));
+                }
+            }
+
+            connection.Close();
+
+            dataGridView1.DataSource = clients;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Items.Add("Сортировка по убыванию");
+            comboBox1.Items.Add("Сортировка по возрастанию");
+            comboBox1.SelectedIndex = 0; // Устанавливаем по умолчанию сортировку по убыванию
+
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.Items.Add("Сумма заказа < 150");
+            comboBox2.Items.Add("Сумма заказа > 150");
+            comboBox2.SelectedIndex = 0; // Устанавливаем по умолчанию фильтрацию по сумме заказа < 150
+
+            for (int j = 0; j < clients.Count; j++)
+            {
+                chart1.Series["Series1"].Points.AddXY(clients[j].ClientID, clients[j].OrderTotal);
+            }
+
+            chart1.Series["Series1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            chart1.Series["Series1"].Label = "#VALX: #VALY";
+            chart1.Series["Series1"].LegendText = "#VALX";
+            chart1.Legends[0].Enabled = true;
+
+            chart2.Series.Clear();
+            chart2.Series.Add("OrderTotal");
+            for (int j = 0; j < clients.Count; j++)
+            {
+                chart2.Series["OrderTotal"].Points.AddXY(clients[j].ClientID, clients[j].OrderTotal);
+            }
+            chart2.ChartAreas[0].AxisX.Interval = 1;
+            chart2.ChartAreas[0].AxisX.LabelStyle.Angle = -45;
+            chart2.ChartAreas[0].AxisX.LabelStyle.Font = new Font("Arial", 8);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                // Сортировка списка клиентов по убыванию суммы заказа и обновление данных в dataGridView2
+                dataGridView2.DataSource = clients.OrderByDescending(c => c.OrderTotal).ToList();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                // Сортировка списка клиентов по возрастанию суммы заказа и обновление данных в dataGridView2
+                dataGridView2.DataSource = clients.OrderBy(c => c.OrderTotal).ToList();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedOption = comboBox2.SelectedItem.ToString();
+
+            List<Client> filteredClients = new List<Client>();
+
+            if (selectedOption == "Сумма заказа < 150")
+            {
+                // Фильтрация списка клиентов по сумме заказа меньше 150 и обновление данных в dataGridView3
+                filteredClients = clients.Where(c => c.OrderTotal < 150).ToList();
+            }
+            else if (selectedOption == "Сумма заказа > 150")
+            {
+                // Фильтрация списка клиентов по сумме заказа больше 150 и обновление данных в dataGridView3
+                filteredClients = clients.Where(c => c.OrderTotal > 150).ToList();
+            }
+
+            dataGridView3.DataSource = filteredClients;
+        }
+
+        class Client
+        {
+            string clientID;
+            string lastName;
+            decimal orderTotal;
+
+            public Client(string clientID, string lastName, decimal orderTotal)
+            {
+                this.clientID = clientID;
+                this.lastName = lastName;
+                this.orderTotal = orderTotal;
+            }
+
+            public string ClientID { get => clientID; set => clientID = value; }
+            public string LastName { get => lastName; set => lastName = value; }
+            public decimal OrderTotal { get => orderTotal; set => orderTotal = value; }
+        }
+
+        private void comboBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string selectedOption = comboBox2.SelectedItem.ToString();
+
+            List<Client> filteredClients = new List<Client>();
+
+            if (selectedOption == "Сумма заказа < 150")
+            {
+                // Фильтрация списка клиентов по сумме заказа меньше 150 и обновление данных в dataGridView3
+                filteredClients = clients.Where(c => c.OrderTotal < 150).ToList();
+            }
+            else if (selectedOption == "Сумма заказа > 150")
+            {
+                // Фильтрация списка клиентов по сумме заказа больше 150 и обновление данных в dataGridView3
+                filteredClients = clients.Where(c => c.OrderTotal > 150).ToList();
+            }
+
+            dataGridView3.DataSource = filteredClients;
+        }
+    }
+}
